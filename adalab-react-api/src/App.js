516803759e83082a8ddeab91f471bbeb250c6234
Wawasisaru <nsaruwawasi@gmail.com>
2023-07-06 13:19:35 +0300

    commit

--- adalab-react-api/src/App.js
+++ adalab-react-api/src/App.js
@@ -1,11 +1,26 @@
-import Login from "./login";
-
+import logo from './logo.svg';
+import './App.css';
+import Products from './Productpage';
+import Login from './Loginpage';
+import Navigationbar from './Navigationbar/navigation';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ProductDetails from './ProductDetails/productdetails';
+// import addProduct from './Addproduct/addproduct';
 function App() {
   return (
-   <div className="App">
-    <Login/>
-   </div>
+    <div>
+      <Navigationbar/>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Login/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/product' element={<Products/>}/>
+        <Route path='/productdetails/:productId' element={<ProductDetails/>}/>
+        {/* <Route path='/addproduct' element={<Addproduct/>}/> */}
+      </Routes>
+      </BrowserRouter>
+      </div>
   );
 }
-
 export default App;
+
--- adalab-react-api/src/Products/index.js
+++ adalab-react-api/src/Products/index.js
@@ -1,26 +1,50 @@
-import React,{useState,useEffect} from "react";
-
-const Products =()=>{ [Products,setProducts] = useState([]);
-    useEffect(()=>{
-
-},[])
-
-const getProducts = async()=>{
-    try{
-        const response = await fetch('https://dummyjson.com/products')
-        const result = await response.json();
-        setProducts(result):
+import React, {useState, useEffect}from "react";
+import './style.css'
+const Products = () =>{
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        (async()=>{
+            await getProducts();
+        })()
+    }, [])
+    console.log({products});
+    const getProducts = async ()=>{
+        try{
+            setLoading(true);
+            const response = await fetch('https://dummyjson.com/products')
+            const result = await response.json();
+            setProducts(result.products);
+            setLoading(false);
+        }
+        catch(error){
+            console.log(error.message);
+        }
     }
-    catch (error){
-     console.log
+    if (loading){
+        return <h2>Loading ... </h2>
     }
-}
-
-
-    return{
-      
-       
-    }
-}
-
+    return(
+        <div>
+            <h1>All Products</h1>
+            {products.map(item => (
+            <div key={item.id}>
+            <h2>{item.title}</h2>
+           </div>
+           ))}
+    </div>
+    );
+};
 export default Products;
+
+
+
+
+
+
+
+
+
+
+
+
